832d417d55e380c05d24d83601e40db416b7a4e3
Kaba CONDE <kabaconde15@gmail.com>
2019-04-10 10:29:26 +0200

    Add service worker

--- js/app.js
+++ js/app.js
@@ -1,66 +1,70 @@
 import { default as Card, initCard } from './components/card/card.js';
 
-(async function(document) {
-  const app = document.querySelector('#app');
-  const skeleton = app.querySelector('.skeleton');
-  const listPage = app.querySelector('[page=list]');
-  skeleton.removeAttribute('active');
-  listPage.setAttribute('active', '');
+(async function (document) {
+  try {
+    const app = document.querySelector('#app');
+    const skeleton = app.querySelector('.skeleton');
+    const listPage = app.querySelector('[page=list]');
+    skeleton.removeAttribute('active');
+    listPage.setAttribute('active', '');
 
-  const data = await fetch('./data/spacex.json');
-  const json = await data.json();
+    const data = await fetch('./data/spacex.json');
+    const json = await data.json();
 
-  const cards = json.map(item => {
-    const constructor = document.createElement('div');
-    constructor.innerHTML = Card;
+    const cards = json.map(item => {
+      const constructor = document.createElement('div');
+      constructor.innerHTML = Card;
 
-    const cardElement = constructor.querySelector('.card');
+      const cardElement = constructor.querySelector('.card');
 
-    initCard(item.image,
-      item.placeholder,
-      item.content.title,
-      item.content.description,
-      cardElement);
-    listPage.appendChild(cardElement);
+      initCard(item.image,
+        item.placeholder,
+        item.content.title,
+        item.content.description,
+        cardElement);
+      listPage.appendChild(cardElement);
 
-    return cardElement;
-    // setTimeout(() => {
-    //   const img = cardElement.querySelector('img');
-    //   img.src = img.dataset.src;
-    //   img.onload = () => {
-    //     // Add the `fade` class to the placeholder
-    //     img.parentNode.querySelector('.placeholder').classList.add('fade');
-    //   }
-    // }, 3000);
-  });
-
-  /**
-   * @see https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-   */
-  const options = {
-    rootMarging : '0px 0px 0px 0px'
-  };
-  const callback = entries => {
-    entries.forEach((entry) => {
-      // If image element in view
-      if (entry.isIntersecting) {
-        // Actualy load image
-        const image = entry.target
-        image.src = image.dataset.src;
-        image.onload = () => {
-          // Add the `fade` class to the placeholder
-          image.parentNode.querySelector('.placeholder').classList.add('fade');
-        }
-      }
+      return cardElement;
+      // setTimeout(() => {
+      //   const img = cardElement.querySelector('img');
+      //   img.src = img.dataset.src;
+      //   img.onload = () => {
+      //     // Add the `fade` class to the placeholder
+      //     img.parentNode.querySelector('.placeholder').classList.add('fade');
+      //   }
+      // }, 3000);
     });
-  };
 
-  const io = new IntersectionObserver(callback, options);
-  // Observe images as they enter the viewport
-  cards.forEach((card) => {
-    const image = card.querySelector('img');
-    io.observe(image);
-  });
+    /**
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+     */
+    const options = {
+      rootMarging: '0px 0px 0px 0px'
+    };
+    const callback = entries => {
+      entries.forEach((entry) => {
+        // If image element in view
+        if (entry.isIntersecting) {
+          // Actualy load image
+          const image = entry.target
+          image.src = image.dataset.src;
+          image.onload = () => {
+            // Add the `fade` class to the placeholder
+            image.parentNode.querySelector('.placeholder').classList.add('fade');
+          }
+        }
+      });
+    };
+
+    const io = new IntersectionObserver(callback, options);
+    // Observe images as they enter the viewport
+    cards.forEach((card) => {
+      const image = card.querySelector('img');
+      io.observe(image);
+    });
+  } catch (error) {
+    console.log(error.toString());
+  }
 })(document);
 
 /*
---
+++ notes.md
@@ -0,0 +1,12 @@
+https://bundlephobia.com/result?p=date%20fns
+https://github.com/developit
+https://whatwebcando.today/
+https://developer.mozilla.org/fr/docs/Glossaire/IIFE
+https://developer.mozilla.org/fr/docs/Web/HTML/Element/template
+https://developer.mozilla.org/fr/docs/Web/API/Intersection_Observer_API
+
+Service worker life cycle
+- download
+- register
+- install
+- activate
